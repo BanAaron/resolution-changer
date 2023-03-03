7d727df7bc1fc0ef8dbdca6b46d120138ce97ac7
Aaron Barratt <aarontbarratt@gmail.com>
2023-03-03 15:12:16 +0000

    Toggle now switches between the current and previously selected resolution

--- main.py
+++ main.py
@@ -13,64 +13,50 @@ class ResolutionChanger:
         self.win_32_con = win32con
         self.dev_mode = self.py_win_types.DEVMODEType()
 
+        self.previous_resolution = ()
+
         self.icon_image = PIL.Image.open("icon.png")
         self.menu = (
             pystray.MenuItem(
                 "Toggle Resolution", self.toggle_resolution, default=True, visible=False
             ),
-            pystray.MenuItem("3840x1080", lambda: self.set_resolution(3840, 1080)),
-            pystray.MenuItem("1920x1080", lambda: self.set_resolution(1920, 1080)),
-            pystray.MenuItem("1280x720", lambda: self.set_resolution(1280, 720)),
+            pystray.MenuItem("3840x1080", lambda: self.change_resolution((3840, 1080))),
+            pystray.MenuItem("1920x1080", lambda: self.change_resolution((1920, 1080))),
+            pystray.MenuItem("1280x720", lambda: self.change_resolution((1280, 720))),
             pystray.MenuItem("Quit", self.on_quit),
         )
-        self.icon = pystray.Icon("Name", self.icon_image, "ResChanger", self.menu)
+        self.icon = pystray.Icon(
+            "Resolution Changer", self.icon_image, "ResChanger", self.menu
+        )
         self.icon.run()
 
-    def get_resolution(self) -> tuple[int, int]:
-        """
-        Get the current resolution of the display
-        :return: Tuple[int, int]
-        """
-        monitor = self.screen_info.get_monitors()[0]
-        return monitor.width, monitor.height
-
-    def set_resolution(self, width: int, height: int):
-        """
-        Sets the display to resolution specified within the parameters
-        :param width: resolution width in pixels
-        :param height: resolution height in pixels
-        """
-        self.dev_mode.PelsWidth = width
-        self.dev_mode.PelsHeight = height
-
+    def change_resolution(self, resolution: tuple[int, int]):
+        self.set_previous_resolution()
+        self.dev_mode.PelsWidth = resolution[0]
+        self.dev_mode.PelsHeight = resolution[1]
         self.dev_mode.Fields = (
             self.win_32_con.DM_PELSWIDTH | self.win_32_con.DM_PELSHEIGHT
         )
         self.win_32_api.ChangeDisplaySettings(self.dev_mode, 0)
 
     def toggle_resolution(self):
-        """
-        Toggles the display resolution between two preset resolutions
-        """
-        resolution = self.get_resolution()
-        if resolution == (3840, 1080):
-            self.dev_mode.PelsWidth = 1920
-        elif resolution == (1920, 1080):
-            self.dev_mode.PelsWidth = 3840
-        else:
-            self.dev_mode.PelsWidth = 3840
-
-        self.dev_mode.Fields = (
-            self.win_32_con.DM_PELSWIDTH | self.win_32_con.DM_PELSHEIGHT
-        )
-        self.win_32_api.ChangeDisplaySettings(self.dev_mode, 0)
+        if len(self.get_previous_resolution()) == 2:
+            self.change_resolution(self.get_previous_resolution())
 
     def on_quit(self):
-        """
-        Quit the program
-        """
         self.icon.visible = False
         self.icon.stop()
+
+    def get_current_resolution(self) -> tuple:
+        screen_info = self.screen_info.get_monitors()[0]
+        return screen_info.width, screen_info.height
+
+    def get_previous_resolution(self) -> tuple:
+        return self.previous_resolution
+
+    def set_previous_resolution(self):
+        screen_info = self.screen_info.get_monitors()[0]
+        self.previous_resolution = (screen_info.width, screen_info.height)
 
 
 if __name__ == "__main__":
