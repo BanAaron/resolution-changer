e249b80ff1c479d81dc5342ca186c941dafc642b
Aaron Barratt <aarontbarratt@gmail.com>
2023-05-23 09:21:12 +0100

    🎉 separated UI code from logic code

--- main.py
+++ main.py
@@ -1,106 +1,48 @@
+import darkdetect
+import PIL.Image
+import pystray
+from resolution_manager import ResolutionManager
+
+
 class ResolutionChanger:
     def __init__(self):
-        import darkdetect
-        import pywintypes
-        import screeninfo
-        import PIL.Image
-        import win32api
-        import win32con
-        import pystray
-
-        self.win_types = pywintypes
-        self.screen_info = screeninfo
-        self.win_32_api = win32api
-        self.win_32_con = win32con
-        self.dev_mode = self.win_types.DEVMODEType()
-
-        self.previous_resolution = ()
-
+        self.res_manager = ResolutionManager()
         self.menu = (
             pystray.MenuItem(
-                "Toggle Resolution", self.toggle_resolution, default=True, visible=False
+                "Toggle Resolution",
+                self.res_manager.toggle_resolution,
+                default=True,
+                visible=False,
             ),
-            pystray.MenuItem("3840x1080", lambda: self.change_resolution((3840, 1080))),
-            pystray.MenuItem("2560x1080", lambda: self.change_resolution((2560, 1080))),
-            pystray.MenuItem("1920x1080", lambda: self.change_resolution((1920, 1080))),
-            pystray.MenuItem("1680x1050", lambda: self.change_resolution((1680, 1050))),
-            pystray.MenuItem("1280x720", lambda: self.change_resolution((1280, 720))),
-            pystray.MenuItem("Quit", self.on_quit),
+            pystray.MenuItem(
+                "3840x1080", lambda: self.res_manager.change_resolution(3840, 1080)
+            ),
+            pystray.MenuItem(
+                "2560x1080", lambda: self.res_manager.change_resolution(2560, 1080)
+            ),
+            pystray.MenuItem(
+                "1920x1080", lambda: self.res_manager.change_resolution(1920, 1080)
+            ),
+            pystray.MenuItem(
+                "Quit", lambda: self.quit()
+            ),
         )
+
         if darkdetect.isDark():
-            self.icon_image = PIL.Image.open("img\\icon_white.png")
+            self.icon_image = PIL.Image.open(r"img\icon_white.png")
         else:
-            self.icon_image = PIL.Image.open("img\\icon_black.png")
+            self.icon_image = PIL.Image.open(r"img\icon_black.png")
 
         self.icon = pystray.Icon(
             "Resolution Changer", self.icon_image, "ResChanger", self.menu
         )
         self.icon.run()
 
-    def change_resolution(self, resolution: tuple[int, int]):
+    def quit(self):
         """
-        Change the resolution of the display
-        :param resolution: Width and height in pixels
-        :return: self
+        exits the application.
         """
-        self.set_previous_resolution()
-        self.dev_mode.PelsWidth = resolution[0]
-        self.dev_mode.PelsHeight = resolution[1]
-        self.dev_mode.Fields = (
-            self.win_32_con.DM_PELSWIDTH | self.win_32_con.DM_PELSHEIGHT
-        )
-        self.win_32_api.ChangeDisplaySettings(self.dev_mode, 0)
-        return self
-
-    def toggle_resolution(self):
-        """
-        toggle between the current resolution and the previous selected
-        :return: self
-        """
-        if len(self.get_previous_resolution()) == 2:
-            self.change_resolution(self.get_previous_resolution())
-        return self
-
-    def notify_resolution(self):
-        """
-        pops up a notification with the current resolution
-        :return: self
-        """
-        resolution = self.get_current_resolution()
-        self.icon.notify(f"{resolution[0]}x{resolution[1]}")
-        return self
-
-    def on_quit(self):
-        """
-        quits the programs main loop
-        :return: None
-        """
-        self.icon.visible = False
         self.icon.stop()
-
-    def get_current_resolution(self) -> tuple:
-        """
-        Gets the current display resolution
-        :return: Tuple of two ints. Width and height in pixels
-        """
-        screen_info = self.screen_info.get_monitors()[0]
-        return screen_info.width, screen_info.height
-
-    def get_previous_resolution(self) -> tuple:
-        """
-        Gets the previously set resolution
-        :return: Tuple of two ints. Width and height in pixels
-        """
-        return self.previous_resolution
-
-    def set_previous_resolution(self):
-        """
-        Sets the previous resolution
-        :return: self
-        """
-        screen_info = self.screen_info.get_monitors()[0]
-        self.previous_resolution = (screen_info.width, screen_info.height)
-        return self
 
 
 if __name__ == "__main__":
