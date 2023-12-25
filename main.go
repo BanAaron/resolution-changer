a7cb24b3431f85588ace4e51fafa5158b90a5a7c
Khephren <aarontbarratt@gmail.com>
2023-12-25 12:41:11 +0000

    exit button working

--- main.go
+++ main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"github.com/getlantern/systray"
 	"github.com/getlantern/systray/example/icon"
+	"log/slog"
 )
 
-var appName = "Resolution Changer"
-
 func onReady() {
+	appName := "Resolution Changer"
 	systray.SetIcon(icon.Data)
 	systray.SetTitle(appName)
 	systray.SetTooltip(appName)
+
+	// menu items
+	res := systray.AddMenuItem("Res", "change res")
+	quit := systray.AddMenuItem("Exit", "exit the program")
+
+	go func() {
+		for {
+			// select listens for all channels
+			select {
+			// when the quit button channel is updated we do something
+			case <-quit.ClickedCh:
+				systray.Quit()
+			case <-res.ClickedCh:
+				fmt.Println("res")
+			}
+		}
+	}()
 }
-func onExit() {}
+
+func onExit() {
+	slog.Info("onExit")
+}
 
 func main() {
 	systray.Run(onReady, onExit)
